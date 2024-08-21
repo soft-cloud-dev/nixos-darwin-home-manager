e952cc873bf1ec953eb86ca340c1ce01267fcbdc
Lukasz D. Tulikowski <lukasz@tulikowski.email>
2024-08-21 21:01:24 +0200

    Updated dependencies and added new packages
    - Updated the 'nixpkgs', 'home-manager', and 'nix-darwin' dependencies to their latest versions.
    - Removed the 'nix-index-database' dependency.
    - Added a new dependency, 'flake-parts'.
    - In the home configuration, removed the import of nix-index-database.hmModules.nix-index.
    - In flake.nix, updated the URLs for nixpkgs, home-manager, and nix-darwin. Also removed references to nix-index-database.
    - In home/default.nix, replaced python3 with python312 and pyenv. Added numerous other packages related to Azure CLI, Typescript, AI/ML tools like libtensorflow and llama libraries.
    - Updated custom Python package from "xontrib-vox" to just "vox".

--- flake.nix
+++ flake.nix
@@ -1,48 +1,35 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-darwin = {
-      url = "github:lnl7/nix-darwin/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nix-index-database = {
-      url = "github:Mic92/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    nix-darwin.url = "github:LnL7/nix-darwin";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, home-manager, nix-darwin, nix-index-database, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-parts, home-manager, nix-darwin, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "aarch64-darwin" "x86_64-darwin" ];
-
       perSystem = { pkgs, system, ... }: {
         formatter = pkgs.nixpkgs-fmt;
-
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [ nixpkgs-fmt just ];
         };
-
-        packages.default = pkgs.writeShellApplication {
-          name = "activate";
-          runtimeInputs = [ inputs.home-manager.packages.${system}.default ];
-          text = ''
-            ${pkgs.lib.getExe inputs.home-manager.packages.${system}.default} switch --flake .#lukasz@${system}
-          '';
+        packages = {
+          default = pkgs.writeShellApplication {
+            name = "activate";
+            runtimeInputs = [ inputs.home-manager.packages.${system}.default ];
+            text = ''
+              ${pkgs.lib.getExe inputs.home-manager.packages.${system}.default} switch --flake .#lukasz@${system}
+            '';
+          };
         };
       };
-
       flake = {
         homeConfigurations."lukasz@aarch64-darwin" = home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.aarch64-darwin;
           extraSpecialArgs = { inherit inputs; };
           modules = [
             ./home
-            nix-index-database.hmModules.nix-index
             {
               home = {
                 username = "lukasz";
@@ -52,7 +39,6 @@
             }
           ];
         };
-
         darwinConfigurations."lukasz-macbook-pro-13" = nix-darwin.lib.darwinSystem {
           system = "aarch64-darwin";
           modules = [
@@ -60,7 +46,6 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.lukasz = import ./home;
             }
             {
               security.pam.enableSudoTouchIdAuth = true;
